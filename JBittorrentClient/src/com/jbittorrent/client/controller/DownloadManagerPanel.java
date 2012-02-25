73f13b850c4bf0a3cc688a0ba7d7ea6956db15a2
rajeevmshr661@gmail.com <rajeevmshr661@gmail.com@6357178c-8836-63be-8f43-cb96b8ce955a>
2012-02-25 18:11:20 +0000

    Add some fuctionality related to GUI such as add,pause,stop,resume,quit

--- JBittorrentClient/src/com/jbittorrent/client/controller/DownloadManagerPanel.java
+++ JBittorrentClient/src/com/jbittorrent/client/controller/DownloadManagerPanel.java
@@ -2,15 +2,24 @@ package com.jbittorrent.client.controller;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-public class DownloadManagerPanel extends JPanel {
+import sun.misc.Cleaner;
+
+import com.jbittorrent.client.mainGui.MainGui;
+
+public class DownloadManagerPanel extends JPanel implements Observer{
 
 	/**
 	 * 
@@ -21,6 +30,9 @@ public class DownloadManagerPanel extends JPanel {
 	 */
 	private DownloadTableModel tableModel;
 	private JTable table;
+	//Currently selected download
+	private Downloader selectedDownloader;
+	private boolean clearing;
 	
 	public DownloadManagerPanel() {
 		super(new GridLayout(1,0));
@@ -33,14 +45,67 @@ public class DownloadManagerPanel extends JPanel {
 			
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-				// TODO Method for valuechanged in table:rajeev
-				
+				tableSelectionChanged();
 			}
 		});
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//TODO Download progress logic using progressbar:rajeev
-		
+		ProgressBarManager renderer=new ProgressBarManager(0, 100);
+		renderer.setStringPainted(true);
+		table.setDefaultRenderer(JProgressBar.class, renderer);
+		table.setRowHeight((int) renderer.getPreferredSize().getHeight());
 		add(scrollPane);
 	}
-
+	public void updateToolbar(){
+		if(selectedDownloader!=null){
+			MainGui.updateToolBarStatus(selectedDownloader.getStatus());
+		}else{
+			MainGui.updateToolBarStatus(100);
+			// fictitious value to indicate no download selected in table
+		}
+	}
+	private void tableSelectionChanged(){
+		if(selectedDownloader!=null)
+			selectedDownloader.deleteObserver(DownloadManagerPanel.this);
+		if(!clearing){
+			selectedDownloader=tableModel.getDownload(table.getSelectedRow());
+			selectedDownloader.addObserver((DownloadManagerPanel.this));
+			updateToolbar();
+		}
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		if(selectedDownloader!=null && selectedDownloader.equals(o))
+			updateToolbar();
+	}
+	public void actionPause(){
+		selectedDownloader.pause();
+		updateToolbar();
+	}
+	public void actionResume(){
+		selectedDownloader.resume();
+		updateToolbar();
+	}
+	public void actionStop(){
+		selectedDownloader.stop();
+		updateToolbar();
+	}
+	public void actionAdd(){
+		//TODO add logic to add url
+		//call verifyUrl method to validate url
+	}
+	private URL verifyUrl(String url){
+		if(!url.toLowerCase().startsWith("http://") || !url.toLowerCase().startsWith("https://"))
+			return null;
+		
+		URL verifiedUrl=null;
+		try {
+			verifiedUrl=new URL(url);
+		} catch (MalformedURLException e) {
+			return null;
+		}
+		if(verifiedUrl.getFile().length()<2 && !verifiedUrl.getFile().endsWith(".torrent"))
+			return null;
+		
+		return verifiedUrl;
+	}
 }
