eb94be63dfcc1a8f49dbe91d857a3c6a38547685
rajeevmshr661@gmail.com <rajeevmshr661@gmail.com@6357178c-8836-63be-8f43-cb96b8ce955a>
2012-02-26 19:19:20 +0000

    Added new functionality related to Create New torrent (CreateNewTorrentDialog.java)

---
+++ trunk/JBittorrentClient/src/com/jbittorrent/client/mainGui/CreateNewTorrentDialog.java
@@ -0,0 +1,71 @@
+package com.jbittorrent.client.mainGui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class CreateNewTorrentDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			CreateNewTorrentDialog dialog = new CreateNewTorrentDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public CreateNewTorrentDialog() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(CreateNewTorrentDialog.class.getResource("/images/icon.png")));
+		setTitle("Create New Torrent");
+		setBounds(100, 100, 496, 600);
+		setLocationRelativeTo(getParent());
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0};
+		gbl_contentPanel.rowHeights = new int[]{0};
+		gbl_contentPanel.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Create and Save as");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
